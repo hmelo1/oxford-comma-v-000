7b0750e9fcff5e5c8c424d75e774097ccf6e6236
Eddie Melo <heriberto@melo.nyc>
2017-12-08 03:28:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-  array.join(",")
+  if array.length > 3 
+    puts "Test"
+    print "Test"
+  elsif array.length == 3
+    puts "Test2"
+    print "Test2"
+  elsif array.length == 2
+    puts "Test3"
+    print "Test3"
+  end    
 end

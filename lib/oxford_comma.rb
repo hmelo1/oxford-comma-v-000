e022d435943e0d1af1cd904585c698a5e2769614
Eddie Melo <heriberto@melo.nyc>
2017-12-08 03:42:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   if array.length > 3
-    return array[0..-3].join(", ") + ", and " + array[-3]
+    return array[0..-3].join(", ") + ", and " + array[-1]
   elsif array.length == 3
     return array[0..-2].join(", ") + ", and " + array[-1]
   elsif array.length == 2
